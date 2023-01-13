e164fb181af58c79c5089bd67afaecc7009df76d
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 10:40:43 +0300

    add new file Family

---
+++ src/Family.java
@@ -0,0 +1,9 @@
+public class Family {
+    public static void main(String[] args) {
+        System.out.println("My family");
+        System.out.println("Alexandru");
+        System.out.println("Veronika");
+        System.out.println("Andreea");
+        System.out.println("Alexandru");
+    }
+}
