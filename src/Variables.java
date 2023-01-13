73329afac68a265191e8691b6ee2a27a63940046
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 11:13:00 +0300

    add new file Variables

---
+++ src/Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String[] args) {
+//        int x, y, z;
+//        x = 5;
+//        y = 5;
+//        z = x + y + 10;
+//
+//        int jon, nick;
+//        jon = 100;
+//        nick = jon * 5;
+//        int sum = jon + nick;
+//        System.out.println(z);
+//        System.out.println(nick);
+//        System.out.println(sum);
+
+
+        int a = 10;
+        int b = 4;
+        int с = a / b;
+        int o = a % b;
+        System.out.println(с);
+        System.out.println(o);
+
+    }
+}
