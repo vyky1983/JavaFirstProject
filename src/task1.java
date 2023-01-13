bed139e3e99da70a45ee8c8dea8557d9477da82e
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 11:39:45 +0300

    add new file task1

--- src/task1.java
+++ src/task1.java
@@ -1,2 +1,22 @@
 public class task1 {
+    public static void main(String[] args) {
+        int day=10000;
+        int year=day/365;
+        int weeks=day%365/30;
+        int leftDay=day-(year*365)-(weeks*30);
+        System.out.println(year);
+        System.out.println(weeks);
+        System.out.println(leftDay);
+
+
+//        int day=365;
+//        int years=day/365;
+//        int months=(day-(years*365))/30;
+//        int leftDays= day-(years*360)-(months*30);
+//
+//        System.out.println(years);
+//        System.out.println(months);
+//        System.out.println(leftDays);
+
+    }
 }
