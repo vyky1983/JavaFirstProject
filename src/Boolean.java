ea4b3426dc925610bf39185ea35579643fb1ef5e
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 20:03:18 +0300

    add new file boolean

--- src/Boolean.java
+++ src/Boolean.java
@@ -1,15 +1,18 @@
 public class Boolean {
     public static void main(String[] args) {
-            int temp = 30;
-            if (temp > 25) {
-                System.out.println(" Кондиционнер включен");
-            } else if (temp<22) {
-                System.out.println(" Кондиционнер выключен");
-            }else {
-                System.out.println(" Кондиционнер ничего не делает");
-            }
+
+        int temp = 30;
+        boolean hot = temp > 25;
+        boolean cold=temp<22;
+        if (hot) {
+            System.out.println(" Кондиционнер включен");
+        } else if (cold) {
+            System.out.println(" Кондиционнер выключен");
+        } else {
+            System.out.println(" Кондиционнер ничего не делает");
         }
     }
+}
 
 
 
