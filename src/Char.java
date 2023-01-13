669564bfbc13dc06986a9b4ada326f23d3155f8d
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 18:26:05 +0300

    add file char

---
+++ src/Char.java
@@ -0,0 +1,9 @@
+public class Char {
+    public static void main(String[] args) {
+        char x = 88;
+        char x1 = 'X';
+        x1++;
+        System.out.println("Это значение x --> " + x);
+        System.out.println("Это значение x1 --> " + x1);
+    }
+}
---
+++ src/Task3.java
@@ -0,0 +1,10 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int a=44;
+        int b=39;
+        int c=15;
+        int d=4;
+        float sum=(float)(a+b+c+d)/4;
+        System.out.println(sum);
+    }
+}
