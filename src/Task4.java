9a152ad953dae7bde85201c8e4097c4de1d04120
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 19:02:23 +0300

    add fileTask4

---
+++ src/Task4.java
@@ -0,0 +1,12 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int ageAlex=44;
+        int ageVyky=39;
+        int ageAndy=15;
+        int ageSan=5;
+        System.out.println("Alexandru : "+ageAlex+"!");
+        System.out.println("Veronika : "+ageVyky+"!");
+        System.out.println("Андрея : "+ageAndy+"!");
+        System.out.println(" Саничка : "+ageSan+"!");
+    }
+}
