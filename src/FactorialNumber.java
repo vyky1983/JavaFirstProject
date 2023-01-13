c759ca19a49f689095ec06926cf0890d8c71857d
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 15:59:02 +0300

    add new file Factorial

---
+++ src/FactorialNumber.java
@@ -0,0 +1,8 @@
+public class FactorialNumber {
+    public static void main(String[] args) {
+     double radius =10.8;
+     double pi =3.14;
+     double area=pi*radius*radius;
+        System.out.println(area);
+    }
+}
--- src/TypeInteger.java
+++ src/TypeInteger.java
@@ -1,5 +1,15 @@
 public class TypeInteger {
     public static void main(String[] args) {
 
+        int a = 5;
+
+        a = a + 1;
+        a += 1;
+        a += 15;
+        a++;
+        a--;
+        System.out.println(a);
+
+
     }
 }
