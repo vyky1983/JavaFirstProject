56aae0c43e7188748cd1ee28bdb9d2904df2e799
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 12:00:09 +0300

    add new file if-else

---
+++ src/ifElse.java
@@ -0,0 +1,12 @@
+public class ifElse {
+    public static void main(String[] args) {
+        int temp = 30;
+        if (temp > 25) {
+            System.out.println(" Кондиционнер включен");
+        } else if (temp<22) {
+            System.out.println(" Кондиционнер выключен");
+        }else {
+            System.out.println(" Кондиционнер ничего не делает");
+        }
+    }
+}
