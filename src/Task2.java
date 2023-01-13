f0b537c1738a4c79ae270cf9091a01ad12b6f0ae
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-13 12:56:37 +0300

    add new file Task2

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,14 @@
 public class Task2 {
-}
+    public static void main(String[] args) {
+       int many=12;
+       if(many>=10){
+           System.out.println(" Пица ");
+       } else if (many>=6) {
+           System.out.println(" Гамбургер ");
+       }
+       else {
+           System.out.println(" Сэндвич ");
+       }
+    }
+    }
+
