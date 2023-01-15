92b150417baeb58d8c7aef13bbb10d5cecf6fa9a
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-15 17:14:47 +0300

    add new inf in file Loopclass

---
+++ src/LoopClass.java
@@ -0,0 +1,13 @@
+public class LoopClass {
+    public static void main(String[] args) {
+        int num=0;
+        while (num<100){
+
+            System.out.print(num+",");
+
+
+        }
+
+
+    }
+}
---
+++ src/Task7.java
@@ -0,0 +1,22 @@
+public class Task7 {
+    public static void main(String[] args) {
+//        int num =0;
+//        while (num<=100){
+//            if(num%2==0){
+//                System.out.println(num);
+//            }
+//            num++;
+//            if(num==15){
+//                break;
+//            }
+//            System.out.println(num);
+////            num+=2;
+//        }
+//
+////    }
+//}
+        int a=0;
+        while (a>0){
+            System.out.println("hello");}
+    }
+    }
