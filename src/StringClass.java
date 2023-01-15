9dbf1b4a05981b1d37dea37d4aa8c2b980ff9050
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-15 15:28:03 +0300

    add files StringClass, Task6

---
+++ src/StringClass.java
@@ -0,0 +1,10 @@
+public class StringClass {
+    public static void main(String[] args) {
+        String Hello = "Hello";
+        String World = " World";
+//        String result = Hello + " " + World+"!!!";
+//
+//        System.out.println(result);
+        System.out.println(Hello + " " + World+"!!!");
+    }
+}
---
+++ src/Task6.java
@@ -0,0 +1,11 @@
+public class Task6 {
+    public static void main(String[] args) {
+        String name ="Vyky";
+        int age=39;
+//        String nameAge=" Привет"+" "+name+" "+" тебе "+" "+age+"лет"+" "+"!!!";
+//        System.out.println(nameAge);
+        System.out.println(" Привет\""+" "+name +"!\n тебе \""+" "+age+"лет"+" "+".");
+
+    }
+
+}
