fb455d871fee46b714f30c7dd8ccbf8dde53bd82
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-15 18:08:18 +0300

    add file Task8

---
+++ src/Task8.java
@@ -0,0 +1,11 @@
+public class Task8 {
+    public static void main(String[] args) {
+        for (int i = 1000; i >= 0; i--) {
+//            System.out.println(i);
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
