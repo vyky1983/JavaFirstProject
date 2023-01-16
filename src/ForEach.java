a24c43b68aab30ac03d1e7d9eddff61e92d607c4
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-16 10:23:58 +0300

    add new file Task11

--- src/ForEach.java
+++
@@ -1,5 +0,0 @@
-public class ForEach {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Task11.java
@@ -0,0 +1,46 @@
+public class Task11 {
+    public static void main(String[] args) {
+        int month = 9;
+        if (month == 1) {
+            System.out.println("Январь");
+        }
+        if (month == 2) {
+            System.out.println("Февраль");
+        }
+        if (month == 3) {
+            System.out.println("Март");
+        }
+        if (month == 4) {
+            System.out.println("Апрель");
+        }
+        if (month == 5) {
+            System.out.println("Май");
+        }
+        if (month == 6) {
+            System.out.println("Июнь");
+        }
+        if (month == 7) {
+            System.out.println("Июль");
+        }
+        if (month == 8) {
+            System.out.println("Август");
+        }
+        if (month == 9) {
+            System.out.println("Сентябрь");
+        }
+        if (month == 10) {
+            System.out.println("Октябрь");
+        }
+        if (month == 11) {
+            System.out.println("Ноябрь");
+        }
+        if (month == 12) {
+            System.out.println("Декабрь");
+        } else if(month<=0 || month>12)
+        {
+            System.out.println(" Данный номер не соответствует месяцу");
+        }
+
+
+    }
+}
