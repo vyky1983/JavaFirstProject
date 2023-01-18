454441c7dfe28c0121f9534658575d3cd8c2d3d0
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-18 11:06:23 +0300

    add new inf2

--- src/Task11_1.java
+++ src/Task11_1.java
@@ -1,2 +1,31 @@
 public class Task11_1 {
+    public static void main(String[] args) {
+        String nameOfMonth = "May";
+        switch (nameOfMonth) {
+            case "December":
+            case "January":
+            case "February":
+                System.out.println("Зима");
+                break;
+            case "Math":
+            case "April":
+            case "May":
+                System.out.println("Весна");
+                break;
+            case "June":
+            case "July":
+            case "August":
+                System.out.println("Лето");
+                break;
+            case "September":
+            case "October":
+            case "November":
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Error");
+
+
+        }
+    }
 }
